67715936420fb6d95aabb6f9ee748e0e3fe1fe2f
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-05-05 20:41:48 +0200

    Milestone1 done

--- main.php
+++ main.php
@@ -1,19 +1,36 @@
 <main>
-    <div class=" dg-main">
-       
 
-        <?php 
-
-        foreach($info_discs as $album) {
-
-        echo('<div class="dg-albums">' . '<img class="dg-poster" src="' . $album['poster'] .' " alt="poster" ' . '/>' . '<h3 class="dg-title">' . $album['title'] . '</h3>' . '<h4 class="dg-author">' . $album['author'] . '</h4>' . '<h4 class="dg-year">' . $album['year'] . '</h4>' . '</div>' );
-
-        }
-
-        ?>
-
+    <!-- sezione select genre -->
+    <div class="dg-select">
+        <h2 class="dg-text-select">Seleziona il genere che preferisci</h2>
+         <select name="genres" class="dg-options">
+        <option value="All">All</option>
+        <option value="Pop">Pop</option>
+        <option value="Metal">Metal</option>
+        <option value="Rock">Rock</option>
+        <option value="Jazz">Jazz</option>
+    </select>
     </div>
 
 
+    <div class="d-flex justify-content-center">
+        <!-- sezione album  -->
+    <div class=" dg-container-albums">
+       
+        <?php 
 
+        foreach($info_discs as $album) { ?>
+
+        <div class="col">
+            <div class="dg-card-album">
+            <img class="dg-poster" src="<?= $album['poster'] ?>" alt="<?= $album['author'] ?>">
+            <h3 class="dg-title"><?= $album['title'] ?></h3>
+            <span class="dg-author"><?= $album['author'] ?></span>
+            <span class="dg-year"><?= $album['year'] ?></span>
+        </div>
+        </div>
+
+       <?php } ?>
+    </div>
+    </div>
 </main>
